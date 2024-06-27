ec8d5f969eb39fea5ce9c4a2bce0e31b44e4ca6c
Vld5 <gritsenko.vld@gmail.com>
2024-06-27 16:30:03 +0500

    dash upd

---
+++ IBM/C8/dash_interactivity_barplot.py
@@ -0,0 +1,50 @@
+# Import required libraries
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+# Read the airline data into pandas dataframe
+airline_data =  pd.read_csv('https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DV0101EN-SkillsNetwork/Data%20Files/airline_data.csv', 
+                            encoding = "ISO-8859-1",
+                            dtype={'Div1Airport': str, 'Div1TailNum': str, 
+                                   'Div2Airport': str, 'Div2TailNum': str})
+                                   
+# Create a dash application
+app = dash.Dash(__name__)
+
+app.layout = html.Div(children=[ html.H1('Total number of flights to the destination state split by reporting airline'
+                                         ,style={'textAlign': 'center', 'color': '#503D36', 'font-size': 40}),
+                                html.Div(["Input Year: ", dcc.Input(id='input-year', value='2010', 
+                                type='number', style={'height':'50px', 'font-size': 35}),], 
+                                style={'font-size': 40}),
+                                html.Br(),
+                                html.Br(),
+                                html.Div(dcc.Graph(id='bar-plot')),
+                                ])
+
+@app.callback( Output(component_id='bar-plot', component_property='figure'),
+               Input(component_id='input-year', component_property='value'))
+# Add computation to callback function and return graph
+def get_graph(entered_year):
+    # Select data based on the entered year
+    df =  airline_data[airline_data['Year']==int(entered_year)]
+    
+    # Group the data by Month and compute the average over arrival delay time.
+    bar_data = df.groupby(['DestState'])['Flights'].sum().reset_index()
+
+    labels = dict(x='Destination state',y='Number of flights')
+    # fig = go.Figure()
+    # fig.add_trace(go.Bar(x=bar_data.DestState, y=bar_data.Flights))
+    # fig.update_layout(xaxis_title=labels['x'], yaxis_title=labels['y'])
+    fig = px.bar(data_frame=bar_data, x='DestState', y='Flights',
+                  labels={'DestState':labels['x'], 'Flights': labels['y']})
+    
+    return fig
+
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server()
